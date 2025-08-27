9334d8789c8446a3c27e2b99cb31bae1a44ce812
tcorderiojr <tedcordeirojr@gmail.com>
2025-08-26 21:06:17 -0300

    login pronto sem funcionalidaes

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,22 +1,26 @@
 import React from "react";
-import {Text, View, Image, TextInput} from "react-native";
+import {Text, View, Image, TextInput, Button, TouchableOpacity} from "react-native";
 import { styles } from "./style";
-import {Logo} from '../../assets/logo.png';
-export default function Login() {
+import Logo from '../../assets/iconUser.png';
+export default function Login({navigation}) {
   return (
     <View style={styles.container}>
       <View style={styles.boxTop}>
-            <Image
-            source={Logo}
-            /><Text>Bem vindo de Volta</Text>
+            <Image source={Logo} style={styles.Image}/>
+            <Text style={styles.TextoPrincipal}> Seja Bem vindo</Text>
       </View>
       <View style={styles.boxMid}>
-            <Text>Endereço de e-mail</Text>
-            <TextInput/>
+            <Text style={styles.TextInput}>Endereço de e-mail</Text>
+            <TextInput style={styles.InputText}/>
       </View>
       <View style={styles.boxBottom}>
-            <Text>senha</Text>
-            <TextInput/>
+            <Text style={styles.TextInput}>Senha</Text>
+            <TextInput style={styles.InputText}/> 
+            <TouchableOpacity style={styles.Button}>
+              <Text style={styles.TextButton}>Entrar</Text>
+            </TouchableOpacity>
+            
+            <Text style={styles.TextInputS}>Esqueci minha senha</Text>
       </View>
     </View>
 
--- src/pages/login/style.js
+++ src/pages/login/style.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from "react-native";
+import { Button, StyleSheet, TextInput } from "react-native";
 
 
 export const styles = StyleSheet.create({
@@ -6,21 +6,68 @@ export const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "#ffffffff",
   },
     boxTop: {
-        width: "100%",
+        width: "80%",        
         height: 200,
-        backgroundColor: "yellow",
     },
+    TextoPrincipal: {
+        top: 20,
+        alignSelf: 'center',
+        color: '#000000',
+        fontSize: 25,
+        fontWeight: 'bold',},
+    Image: {
+        width: 230,
+        top: 30,
+        height: 130,
+       resizeMode: 'contain',
+      alignSelf: 'center',},
     boxMid: {
-        width: "100%",
-        height: 200,
-        backgroundColor: "red",
+        width: "80%",
+        height: 100,
     },
+    TextInput: {
+        top: 20,
+        left: 20,
+        color: '#000',
+        fontSize: 14,
+        alignSelf: 'flex-start',
+        fontWeight: 'bold',},
+    InputText: {
+        width: "90%",
+        height: 40,
+        borderRadius: 7,
+        borderWidth: 1,
+        borderColor: '#000',
+        alignSelf: 'center',
+        top: 30,},    
     boxBottom: {      
-        width: "100%",  
+        width: "80%",
+        bottom: 20,  
         height: 200,
-        backgroundColor: "green",
-
-  }
+  },
+    TextInputS: {
+        top: 70,
+        left: 20,
+        color: '#082f55ed',
+        fontSize: 14,
+        alignSelf: 'flex-start',
+        fontWeight: 'bold',},
+    Button: {
+        top:60,
+        borderRadius: 7,
+        alignSelf: 'center',
+        width: "90%",
+        height: 40,
+        backgroundColor: '#082f55ed',
+        },
+    TextButton: {
+        flex: 1,
+        alignSelf: 'center',
+        justifyContent: 'center',  
+        color: '#fff',
+        top: 5,
+        fontSize: 18,},
 });
